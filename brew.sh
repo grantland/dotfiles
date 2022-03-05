4d31442431968f6039531216b1cc5d4bc1cb2d93
Grantland Chew <grantlandchew@gmail.com>
2022-03-04 18:35:09 -0800

    Clean up

--- brew.sh
+++ brew.sh
@@ -13,20 +13,34 @@ brew install bash
 brew install bash-completion2
 
 # Install some apps
+brew install mas
+brew install --cask the-unarchiver
+
 brew install bitwarden-cli
+
+brew install --cask syncthing
+brew install --cask google-drive
+
 brew install --cask alfred
 brew install rectangle
 brew install --cask dozer
 brew install --cask monitorcontrol
 #brew install unclack
 brew install --cask krisp
+
 brew install --cask sublime-text
+
+brew install --cask signal
+brew install --cask whatsapp
 brew install --cask discord
-brew install mas
+
+brew install --cask android-file-transfer
 brew install scrcpy
 brew install --cask vysor
-brew install --cask syncthing
-brew install --cask qlvideo
-brew install ncdu
+
 brew install --cask ultimaker-cura
 brew install openscad
+
+brew install --cask qlvideo
+brew install ncdu
+
--- mas.sh
+++ mas.sh
@@ -1,6 +1,4 @@
 mas install 1352778147  # Bitwarden
 mas install 497799835   # Xcode
-mas install 1480068668  # Messenger
-mas install 425424353   # The Unarchiver
 mas install 998361254   # Toothpicks
 mas install 980888073   # Crypto Pro
